3a8cbb5a17de010f4c0bbdceb9a5bd92e6dc81f2
mathieu hollande <mathieuhollande@hotmail.fr>
2022-04-01 15:11:36 -0400

    clean up & organise the js file

--- Build Role Playing game Part 1/index.js
+++ Build Role Playing game Part 1/index.js
@@ -168,55 +168,14 @@ app working.
 */
 
 
-const characterData = {
-  hero: {
-    elementId: "hero",
-    name: "Wizard",
-    avatar: "images/wizard.png",
-    health: 60,
-    diceCount: 3
-  },
-  monster: {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "images/orc.png",
-    health: 10,
-    diceCount: 1
-  }
-}
-
-function Character(data) {
-  Object.assign(this, data)
-
-  this.getDiceHtml = function (diceCount) {
-    return getDiceRollArray(diceCount).map(function (num) {
-      return `<div class="dice">${num}</div>`
-    }).join('')
-  }
-
-  this.getCharacterHtml = function () {
-    const { elementId, name, avatar, health, diceCount } = this;
-    let diceHtml = this.getDiceHtml(diceCount);
-
-    return `
-            <div class="character-card">
-                <h4 class="name"> ${name} </h4>
-                <img class="avatar" src="${avatar}" />
-                <div class="health">health: <b> ${health} </b></div>
-                <div class="dice-container">
-                    ${diceHtml}
-                </div>
-            </div>`;
-  }
-}
-
+import characterData from '/data.js'
+import Character from '/Character.js'
 
 function render() {
   document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
   document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
 }
-wizard
-/*Does something here need to change?*/
+
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
 render()
